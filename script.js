3885a769cbc58f7b0b2ebfeea1351f3660b53957
slady <csl.bunny.2207@gmail.com>
2023-05-10 00:32:25 +0200

    geolocation

--- script.js
+++ script.js
@@ -12,12 +12,18 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // Geolocation API
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude}.${longitude},15z?hl=sr` // copy url from google maps, and replace numbers with latitude and longitude using template literal like this.. right click on this link in console and open in new tab to see map
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
 
-navigator.geolocation.getCurrentPosition(
-  function () {},
-  function () {
-    alert('Could not get your position');
-  }
-);
-
-// getCurrentPosition() takes as arguments 2 functions, first is on seccess (when map loads), second is if map doesn't load
+// getCurrentPosition() takes as arguments 2 callback functions, first is on seccess (when map loads), second is if map doesn't load

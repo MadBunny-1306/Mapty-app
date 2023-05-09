c483f96b2e7649fce5b8bb74f55e996bdbb17c28
slady <csl.bunny.2207@gmail.com>
2023-05-10 00:13:15 +0200

    mapty app starter files

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// Geolocation API
+
+navigator.geolocation.getCurrentPosition(
+  function () {},
+  function () {
+    alert('Could not get your position');
+  }
+);
+
+// getCurrentPosition() takes as arguments 2 functions, first is on seccess (when map loads), second is if map doesn't load
